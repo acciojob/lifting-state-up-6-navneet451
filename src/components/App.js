18f517b550fe008a6cbe3493b45854e5aba8234d
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-11-18 20:32:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 const App = () => {
+  // Initial todos state
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a Todo App", completed: false },
+    { id: 3, text: "Master JavaScript", completed: false },
+  ]);
+
+  // Function to handle completion of a todo item
+  const handleComplete = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: true } : todo
+      )
+    );
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Todo List</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
